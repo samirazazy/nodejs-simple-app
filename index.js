21776209e124824313ff23a39de0a33ec4aa2a5a
samirazazy <samiralazazy@gmail.com>
2020-03-23 11:47:50 +0200

    ass tamplates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require('http');
+const url = require('url');
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -25,11 +26,61 @@ const http = require('http');
 
 ///////////////////////////////
 // server
+const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
+const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8')
+
+const replacTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%QUANTATY%}/g, product.quantity);
+  output = output.replace(/{%NUTRATION%}/g, product.nutrients);
+  output = output.replace(/{%DESCREPTION%}/g, product.descreption);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+
+  if (!product.organic) output = output.replace(/{%NOT-ORGANIC%}/g, 'not - organic');
+  return output;
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
-  // console.log(req;
-  res.end('heloo from the server?!...');
+  // console.log(req);
+  // console.log(req.url);
+  const pathNam = req.url;
+
+  //overview
+  if (pathNam === '/' || pathNam === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text.html' });
+
+    const cardHtml = dataObj.map(el => replacTemplate(card, el));
+    const output = overview.replace('{%PRODUCT_CARD%}', cardHtml);
+    res.end(output);
+
+    //product
+  } else if (pathNam === '/product') {
+    res.end('heloo from product');
+
+    //api
+  } else if (pathNam === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(data);
+
+    //not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end('<h1>page not found!</h1>');
+  }
+
 });
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('listining to req on 8000...');
+  console.log('listining to req on 8000');
 });
